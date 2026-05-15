5b7b87e486e2ba4f7c4ed5a9c86b3c848c2e9331
Sumant-Kumar-Chandrawanshi <sumantkumarchandrawanshi@gmail.com>
2026-05-15 20:05:09 +0530

    Entity class

--- src/main/java/com/panther/sb_jpql_practice1/Employee.java
+++ src/main/java/com/panther/sb_jpql_practice1/Employee.java
@@ -1,5 +1,6 @@
 package com.panther.sb_jpql_practice1;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ public class Employee {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	@NotNull()
+	@Column(nullable = false)
 	private String name;
 	@Email()
 	private String email;
